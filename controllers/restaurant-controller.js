99801db8dbc85af568036378c34e003b3f5a23aa
TallTall22 <syuns1205@gmail.com>
2023-04-05 19:40:54 +0800

    add category_tabs pagination

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,19 +1,36 @@
 const { Restaurant, Category } = require('../models')
-
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    Restaurant.findAll({
-      include: Category,
-      raw: true,
-      nest: true
-    })
-      .then(restaurants => {
-        const data = restaurants.map(r => ({
+  getRestaurants: (req, res, next) => {
+    const DEFAULT_LIMIT = 9
+    const categoryId = Number(req.query.categoryId) || ''
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const page = Number(req.query.page) || 1
+    const offset = getOffset(limit, page)
+    Promise.all([
+      Restaurant.findAndCountAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        limit,
+        offset,
+        raw: true,
+        nest: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+
+      .then(([restaurants, categories]) => {
+        const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
-        return res.render('restaurants', { restaurants: data })
+        return res.render('restaurants', {
+          restaurants: data, categories, categoryId, pagination: getPagination(limit, page, restaurants.count)
+        })
       })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     const id = req.params.id
