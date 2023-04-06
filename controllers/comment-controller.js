31489a389359d96aa25324f0360e1012c2e23f86
TallTall22 <syuns1205@gmail.com>
2023-04-06 17:43:40 +0800

    add comment

---
+++ controllers/comment-controller.js
@@ -0,0 +1,36 @@
+const { User, Restaurant, Comment } = require('../models')
+
+const commentController = {
+  postComment: (req, res, next) => {
+    const { restaurantId, text } = req.body
+    const userId = req.user.id
+    if (!text) throw new Error('Comment text is required!')
+    Promise.all([
+      Restaurant.findByPk(restaurantId),
+      User.findByPk(userId)
+    ])
+      .then(([restaurant, user]) => {
+        if (!restaurant) throw new Error('The Restaurant is not existed')
+        if (!user) throw new Error('This user is not existed')
+        return Comment.create({
+          text,
+          restaurantId,
+          userId
+        })
+      })
+      .then(() => res.redirect(`/restaurants/${restaurantId}`))
+      .catch(err => next(err))
+  },
+  deleteComment: (req, res, next) => {
+    const id = req.params.id
+    Comment.findByPk(id)
+      .then(comment => {
+        if (!comment) throw new Error("Comment didn't exist!")
+        return comment.destroy()
+      })
+      .then(deleteComment => res.redirect(`/restaurants/${deleteComment.restaurantId}`))
+      .catch(err => next(err))
+  }
+}
+
+module.exports = commentController
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 
-const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/comment-controller')
 
 const { adminAuthenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
@@ -12,16 +13,22 @@ const { authenticated } = require('../middleware/auth')
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+
 router.get('/logout', userController.logout)
 router.use('/admin', adminAuthenticated, admin)
-router.get('/restaurants/:id/dashboard', restController.getDashboard)
-router.get('/restaurants/:id', restController.getRestaurant)
+
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
-router.use('/', generalErrorHandler)
 
+router.delete('/comments/:id', authenticated, commentController.deleteComment)
+router.post('/comments', authenticated, commentController.postComment)
+
+router.use('/', generalErrorHandler)
 module.exports = router
